2fb4ce66e56be26e949ed4f47cef0bddda906419
RichardEstime <richardjestime@yahoo.com>
2021-06-10 13:11:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution in this file
 //Define a employee variable and assign it to an object containing name, and street address keys
 
-const updatedEmployeeWithKeyAndValue(employee,key,value) {
-    return Object.assign({},employee, {[key]:value})
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    return Object.assign({},employee, {[key]:value});
 }
 
 //function updateEmployeeWithKeyAndValue(key,value) {
