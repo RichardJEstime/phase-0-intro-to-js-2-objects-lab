7fa75f91d78966c72c58150144e8c747d9aa4955
RichardEstime <richardjestime@yahoo.com>
2021-06-10 16:08:39 -0400

    commit tree b6f5a60ca300f019874fb22ceba844be0ed811df

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 // Write your solution in this file
 //Define a employee variable and assign it to an object containing name, and street address keys
-
-function updateEmployeeWithKeyAndValue(employee,key,value) {
-    const result = Object.assign({},employee, {[key]:value});
+const employee = {
+   name: " ",
+   streetAddress: " "
+ };
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const result = Object.assign({}, employee, { [key]: value });
         console.log(result)
+        return result;
+}
+
+// updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+//returns employee without the delete key/value pair
+//modifies the original employee
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+         return employee;
+}
+
+//1) deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+//2) does not modify the original employee (it is non-destructive)
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...key}
+    delete newEmployee.key;
+    return newEmployee;
 }
 
 
-//function updateEmployeeWithKeyAndValue(key,value) {
-//  return {key:value}
-//}
+function destructivelyDeleteFromEmployeeByKey(employee,name) {
+    const newEmployee = ['name']
+}
+//       3) returns employee without the delete key/value pair
+//       4) modifies the original employee
+
